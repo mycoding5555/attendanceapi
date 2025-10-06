250d64d1754e97c16cacea1e09c89bf919ebea26
mycoding5555 <mycoding5555@gmail.com>
2025-10-06 14:43:17 +0700

    #Create Project

---
+++ src/test/kotlin/kotlinapi/AttendanceApi/AttendanceApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlinapi.AttendanceApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AttendanceApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
